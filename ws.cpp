926fb9fa21fb781ff7de14f40f88f40cf3c4d6db
ki6090 <ki6090@kaist.ac.kr>
2024-08-06 13:49:02 +0900

    commit

--- ws.cpp
+++ ws.cpp
@@ -16,32 +16,22 @@ static void divide_gemm_ws(config* config, vector<tuple<int, int, int>> *computa
     int k = config->mnk.k;
     int a_w = config->array_w;
     int a_h = config->array_h;
-
-    /* Divide weight matrix. */
     int window_x;      
     int left_col = col;
     int left_row = row;
-
-    /* Initialize. */
     int window_y = a_h;  
 
-    /* Iterate. */
     while (window_y <= row) {
-        /* Initialize. */
         window_x = a_w;
-        /* Iterate. */
         while (window_x <= col) {
             computations->push_back(make_tuple(a_h, a_w, k));
-            /* Update. */
             window_x += a_w;
             left_col -= a_w;
         }
         if (left_col)
             computations->push_back(make_tuple(a_h, left_col, k));
-        /* Update. */
         left_row -= a_h;
         window_y += a_h;
-        /* Initialize. */
         left_col = col;
     }
     if (left_row) {
@@ -96,7 +86,6 @@ static void simulate_prefill_gemm_ws(config *config, vector<tuple<int, int, int>
         cout << "[" << get<0>(i) << "x" << get<1>(i) << "]x[" << get<1>(i) << "x" << get<2>(i) << "]\n";
         int weights = get<0>(i) * get<1>(i);
         int ifmaps = get<1>(i) * get<2>(i);
-        /* Filter. */
         if (weight_sum < filter_sram_size) {
             int put = weights < (filter_sram_size - weight_sum) ? weights : (filter_sram_size - weight_sum);
             weight_vp->push_back(put);
@@ -104,7 +93,7 @@ static void simulate_prefill_gemm_ws(config *config, vector<tuple<int, int, int>
         else {
             weight_vp->push_back(0);
         }
-        /* Ifmap. */
+
         if (ifmaps <= ifmap_sram_size) {
             ifmap_vp->push_back(ifmaps);
         }
@@ -132,10 +121,10 @@ static void simulate_filling_weights_gemm_ws(config *config, result *result, tup
     int filter_sram_size = config->filter_sram_size;
     int off_chip_memory_cycles = config->off_chip_memory_cycles;
     int cycles = *cycles_p;
+
     on_chip_weights = weight_vp->front();
     weight_vp->pop_front();
-
-    /* Weights Filling Simulation. */
+    
     int acc = 0;
     int add_acc = m;
     int weight_cycles = config->array_h;
@@ -146,22 +135,17 @@ static void simulate_filling_weights_gemm_ws(config *config, result *result, tup
             acc += add_acc;
         }
         if (on_chip_weights < 0) {
-            /* Stall Updates. */
             left_weights += add_acc;
             on_chip_weights = filter_sram_size <= left_weights ? filter_sram_size : left_weights;
-
-            /* Stall Cycles. */
             stall_cycles += off_chip_memory_cycles;
             stalls.push_back(make_tuple(cycles, cycles + off_chip_memory_cycles - 1));
             cycles += off_chip_memory_cycles;
 
-            /* Updates. */
             left_weights -= add_acc;
             on_chip_weights -= add_acc;
         }
         cycles++;   
     }
-    /* Weights Finish. */
     assert(on_chip_weights == 0);
     assert(left_weights == 0);
     assert(acc == weights);
@@ -181,7 +165,6 @@ static void simulate_filling_weights_gemm_ws(config *config, result *result, tup
 }
 
 static void simulate_activation_gemm_ws(config *config, result *result, tuple<int, int, int> i, deque<int> *ifmap_vp, int *cycles_p, int *stall_cycles_p, int *ofmap_weights_p) {
-/* Ifmap Initialize. */
     int m = get<0>(i);
     int n = get<1>(i);
     int k = get<2>(i);
@@ -201,28 +184,21 @@ static void simulate_activation_gemm_ws(config *config, result *result, tuple<in
     on_chip_weights = ifmap_vp->front();
     ifmap_vp->pop_front();
 
-    /* Ofmap Initialize. */
     int ofmap_acc = 0;
-    
-    /* Move Results to Off-Chip if exists. */
     if (ofmap_weights > 0 && off_chip_memory_cycles) {
-        /* Stall Cycles. */
         stall_cycles += off_chip_memory_cycles;
         cout << "Stalls(Ofmap): ";
         cout << cycles << "~" << cycles + off_chip_memory_cycles - 1 << "(" << off_chip_memory_cycles << "cycles)\n";
         cycles += off_chip_memory_cycles;  
-        /* Initialize Ofmap Weights. */
         *ofmap_weights_p = 0;
         ofmap_weights = 0;
     }
 
-    /* Activation Simulation. */
     int acc = 0;
     int add_acc = 0;
     int ifmap_cycles = config->array_h + (config->array_w - 1) + k;
     start_cycles = cycles;
     for (int c = 1; c <= ifmap_cycles; c++) {
-        /* add_acc Updates. */
         if (c <= n) {
             add_acc += 1;
         }
@@ -235,26 +211,20 @@ static void simulate_activation_gemm_ws(config *config, result *result, tuple<in
             acc += add_acc;
         }
         if (on_chip_weights < 0) {
-            /* Stall Updates. */
             left_weights += add_acc;
             on_chip_weights = ifmap_sram_size <= left_weights ? ifmap_sram_size : left_weights;
-
-            /* Stall Cycles. */
             stall_cycles += off_chip_memory_cycles;
             stalls.push_back(make_tuple(cycles, cycles + off_chip_memory_cycles - 1, false));
             cycles += off_chip_memory_cycles;
 
-            /* Updates. */
             left_weights -= add_acc;
             on_chip_weights -= add_acc;
         }
-        /* Results Goes to Ofmap every get<1>(i) cycles. */
         if (c > ifmap_cycles - k) {
             ofmap_weights += m;
             ofmap_acc += m;
             if (ofmap_weights > ofmap_sram_size) {
                 ofmap_weights = m;
-                /* Stall Cycles. */
                 stall_cycles += off_chip_memory_cycles;
                 stalls.push_back(make_tuple(cycles, cycles + off_chip_memory_cycles - 1, true));
                 cycles += off_chip_memory_cycles;
